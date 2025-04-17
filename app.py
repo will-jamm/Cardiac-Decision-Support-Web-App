24ed652d56eb5db8ad3a00fdd30fb5f3b6c0f03f
will-jamm <wiljami.ventila@tuni.fi>
2025-04-17 19:04:05 +0300

    add chart plot for 10-year ASCVD risk

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from src.fhir_client import FHIRClient
+import numpy as np
 import streamlit as st
+import plotly.graph_objects as go
 from src import charts
 from src.ascvd_risk_calculator import ASCVDRiskCalculator
 
@@ -46,7 +48,7 @@ class DecisionSupportInterface():
                     
         return results
 
-    def display_patient_information(self, demographics, weight, height):
+    def display_patient_information(self, demographics, weight, height, cholesterol_data):
 
         if len(weight) == 0:
             latest_weight = "No data"
@@ -59,42 +61,117 @@ class DecisionSupportInterface():
         else:
             latest_height = height[-1]['display']
             height_date = height[-1]['formatted_date']
+        
+        total_cholesterol = cholesterol_data['total_cholesterol']
+        hdl_cholesterol = cholesterol_data['hdl_cholesterol']
 
         if demographics:
             given, surname, birthdate, age, sex = demographics
-            
             
             st.title(f"{given} {surname}")                
 
             col1, col2 = st.columns(2)
             with col1:
-                with st.container(border=True):  # Light blue background
+                with st.container(border=True): 
                     st.subheader("Patient Information")
                     st.write(f"**Age:** {age} years")
                     st.write(f"**Sex:** {sex}")
                     st.write(f"**Date of Birth:** {birthdate}")
 
             with col2:
-                with st.container(border=True):  # Light green background
-                    st.subheader("Latest Measurements")
+                with st.container(border=True):
+                    st.subheader("Patient Vitals")
+                    
                     if latest_height == "No data":
-                        st.write(f"**Height:** {latest_height}")
+                        st.write(f"**Height:** **{latest_height}**")
                     else:
-                        st.write(f"**Height:** {latest_height} (measured: **{height_date}**)")
+                        st.write(f"**Height:** **{latest_height}** ({height_date})")
         
                     if latest_weight == "No data":
-                        st.write(f"**Weight:** {latest_weight}")
+                        st.write(f"**Weight:** **{latest_weight}**")
                     else:
-                        st.write(f"**Weight:** {latest_weight} (measured: **{weight_date}**)")
+                        st.write(f"**Weight:** **{latest_weight}** ({weight_date})")
 
+                    st.write(f"**Total cholesterol:** {total_cholesterol} mg/dL")
+                    st.write(f"**HDL cholesterol:** {hdl_cholesterol} mg/dL")
 
+    def _create_risk_chart(self, risk_value, risk_categories):
+
+        categories = list(risk_categories.keys())
+        thresholds = list(risk_categories.values())
+        colors = ['#E3F2FD', '#90CAF9', '#42A5F5', '#1565C0']
+        ranges = [0] + thresholds + [100]
+
+        fig = go.Figure()
+        
+        for i in range(len(colors)):
+            fig.add_trace(go.Bar(
+                x=[ranges[i+1] - ranges[i]],
+                y=[''],
+                orientation='h',
+                marker=dict(color=colors[i]),
+                base=ranges[i],
+                showlegend=False,
+                hoverinfo='none'
+            ))
+        
+        for i in thresholds:
+            fig.add_annotation(
+            x=i,
+            y=0.5,
+            text=f"{i:.1f}%",
+            showarrow=False,
+            font=dict(size=13, color='black')
+        )
+
+        fig.add_annotation(
+            x=risk_value,
+            y=0.5,
+            text=f"{risk_value:.1f}%",
+            showarrow=False,
+            font=dict(size=13, color='black')
+        )
+        
+
+        fig.add_trace(go.Scatter(
+            x=[risk_value],
+            y=[0],
+            mode='markers',
+            marker=dict(color='black', size=15, symbol='triangle-up'),
+            showlegend=False
+        ))
+        
+        for trace in fig.data:
+            if isinstance(trace, go.Bar):
+                trace.marker.line = dict(color='black', width=2)
+
+        fig.update_layout(
+            title=f"10-Year ASCVD Risk:",
+            height=350,
+            width=800,
+            margin=dict(l=50, r=50, t=80, b=80),
+            xaxis=dict(
+            range=[0, risk_value],
+            showgrid=False
+            ),
+            yaxis=dict(
+            showticklabels=True,
+            showgrid=False
+            ),
+            barmode='stack',
+            plot_bgcolor='white'
+        )
+        
+        return fig
+    
     def display_risk_score(self, demographics, cholesterol_data, systolic_bp, is_treated_bp, is_smoker, has_diabetes):
+
         if not cholesterol_data or not systolic_bp or not demographics:
             st.info("Not enough data to calculate ASCVD risk.")
             return
 
         total_chol = cholesterol_data.get("total_cholesterol")
-        print(total_chol)
+   
         hdl_chol = cholesterol_data.get("hdl_cholesterol")
 
         if not total_chol or not hdl_chol:
@@ -115,10 +192,22 @@ class DecisionSupportInterface():
             hasDiabetes=has_diabetes
         )
 
+        risk_categories = {
+            "Low Risk": 5.0,
+            "Moderate Risk": 7.5,
+            "High Risk": 20.0
+        }
+        
         if isinstance(risk, dict):  # handle validation error
             st.warning(risk['message'])
         else:
-            st.success(f"10-Year ASCVD Risk Score: **{risk:.2f}%**")
+            fig = self._create_risk_chart(risk, risk_categories)
+
+            st.plotly_chart(
+                fig
+            )
+            st.write(f"**10-Year ASCVD Risk Score: {risk:.1f}%**")
+            
 
     
     def dashboard(self):
@@ -168,7 +257,7 @@ class DecisionSupportInterface():
                         "hdl_cholesterol": hdl_chol
                     }
 
-                    self.display_patient_information(demographics=demographics, weight=weight_history, height=height_history)
+                    self.display_patient_information(demographics=demographics, weight=weight_history, height=height_history, cholesterol_data=cholesterol_data)
 
 
                     row1_col1, row1_col2 = st.columns(2)
@@ -189,9 +278,10 @@ class DecisionSupportInterface():
                     with row2_col2:
                         charts.plot_heart_rate(hr_history, demographics, self.client)
 
-                    
+                    st.divider()
+                    st.title("ASCVD Risk Assessment")
                     self.display_risk_score(demographics, cholesterol_data, systolic_bp, is_treated_bp, is_smoker,
-                                            has_diabetes)
+                                            has_diabetes) 
 
             else:
                 st.warning("No matching patients found")
